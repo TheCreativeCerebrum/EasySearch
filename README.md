2f21f3ffc07074f30152e8e428bb0178fa354862
SheaDLady <SheaDahl85@gmail.com>
2023-07-06 00:45:47 -0500

    Prompted Bard to update code to match LinkedIn code

--- README.md
+++ README.md
@@ -21,9 +21,10 @@ The dictionary is then used to find jobs that use Job Query Controls against Wor
 ## How to use the code
 
 1. Save the code as a Python file.
-2. Run the code from the command line.
+2. Run the code from the command line with, "python main.py"
 3. Enter the requested information when prompted.
 4. The code will print the results of the search to the console.
+5. To test the code from the command line type," python -m unittest test_get_linkedin_data.py"
 
 ## Requirements
 
--- get_linkedin_data.py
+++ get_linkedin_data.py
@@ -19,21 +19,46 @@ def get_linkedin_data(linkedin_url):
 
   data = {}
 
+  # Check if the soup object is None.
+
+  if soup is None:
+    raise ValueError("Invalid LinkedIn URL")
+
   # Get personal contact information.
 
-  data["first_name"] = soup.find("span", class_="full-name").text.split(" ")[0]
-  data["last_name"] = soup.find("span", class_="full-name").text.split(" ")[1]
-  data["phone_number"] = soup.find("li", class_="pv-contact-info__phone").text
-  data["email"] = soup.find("li", class_="pv-contact-info__email").text
-  data["address"] = soup.find("li", class_="pv-contact-info__address").text
+  full_name = soup.find("span", class_="full-name").text
+  if full_name is None:
+    full_name = input("Enter your full name: ")
+    data["full_name"] = full_name.split(" ")[0]
+    data["last_name"] = full_name.split(" ")[1]
+  else:
+    data["full_name"] = full_name.split(" ")[0]
+    data["last_name"] = full_name.split(" ")[1]
+
+  phone_number = soup.find("li", class_="pv-contact-info__phone")
+  if phone_number is None:
+    phone_number = input("Enter your phone number: ")
+    data["phone_number"] = phone_number
+
+  email = soup.find("li", class_="pv-contact-info__email")
+  if email is None:
+    email = input("Enter your email address: ")
+    data["email"] = email
+
+  address = soup.find("li", class_="pv-contact-info__address")
+  if address is None:
+    address = input("Enter your address: ")
+    data["address"] = address
 
   # Get professional summary.
 
-  data["professional_summary"] = soup.find("div", class_="summary").text
+  professional_summary = soup.find("h1", class_="pv-profile-top-card__headline").text
+  data["professional_summary"] = professional_summary
+
 
   # Get core competencies.
 
-  data["core_competencies"] = [
+  data["skills"] = [
       skill.text for skill in soup.find_all("li", class_="pv-skills-section__skill-item")
   ]
 
@@ -54,18 +79,23 @@ def get_linkedin_data(linkedin_url):
 
   # Get work experience.
 
-  data["work_experience"] = []
-  for work_experience_item in soup.find_all("div", class_="pv-work-experience-entity"):
-    employer = work_experience_item.find("h3", class_="pv-work-experience-entity__company").text
-    position = work_experience_item.find("h4", class_="pv-work-experience-entity__position").text
-    length_at_company = work_experience_item.find("span", class_="pv-work-experience-entity__dates").text
-    work_experience = work_experience_item.find("div", class_="pv-work-experience-entity__description").text
-    data["work_experience"].append({
-        "employer": employer,
-        "position": position,
-        "length_at_company": length_at_company,
-        "work_experience": work_experience,
+  data["experience"] = []
+  for experience_item in soup.find_all("div", class_="pv-work-experience-entity"):
+    companyName = experience_item.find("h3", class_="pv-work-experience-entity__company").text
+    title = experience_item.find("h4", class_="pv-work-experience-entity__position").text
+    start_date = experience_item.find("span", class_="pv-work-experience-entity__dates").text.split("–")[0]
+    end_date = experience_item.find("span", class_="pv-work-experience-entity__dates").text.split("–")[1]
+    description = experience_item.find("div", class_="pv-work-experience-entity__description").text
+    data["experience"].append({
+        "companyName": companyName,
+        "title": title,
+        "start_date": start_date,
+        "end_date": end_date,
+        "description": description,
     })
+
+
+#get Query controls
 
   salary = input("Enter your desired salary: ")
   benefits = input("Enter your desired benefits: ")
